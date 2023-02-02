44f019dbeeafde16860470d29dd03637a9f0fbd6
bhumivegad <110536859+bhumivegad@users.noreply.github.com>
2023-02-02 17:13:53 +0530

    Add files via upload

---
+++ static1.java
@@ -0,0 +1,19 @@
+//static
+
+class static1
+{
+	static int x=100;
+	static void disp()
+	{
+		System.out.println("disp");
+	}
+	public static void main(String args[])
+	{
+		disp();
+		System.out.println("x ="+x);
+	}
+}
+
+//o.p.
+//disp
+//x=100

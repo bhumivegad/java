188dafd934bae368d87fd082562e4cec6a472002
bhumivegad <110536859+bhumivegad@users.noreply.github.com>
2023-03-20 14:58:49 +0530

    Add files via upload

--- read1.java
+++ read1.java
@@ -1,21 +1,8 @@
 import java.io.*;
-public class read1
+public class read1 
 {
-	public static void main(String args[])
-	{
-		try
-		{
-			int b;
-			FileInputStream fis = new FileInputStream("c:\\bhumi\\java\\test1.txt");
-			b = fis.read();
-			System.out.println((char)b);
-			fis.close();
-		}
-		catch(Exception e)
-		{
-			System.out.println("ERROR");
-		}
-    }
-}	
+    public static void main(String[] args) 
+    {
 
-//o.p. c
+    }   
+}
